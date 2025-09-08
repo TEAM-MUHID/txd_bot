c138392278e53f8609333e6439b9548f1311a63e
I.A.M.S <islammuhid962@gmail.com>
2025-09-08 15:09:15 +0600

    Update Google.js

--- Script/commands/Google.js
+++ Script/commands/Google.js
@@ -5,7 +5,7 @@ module.exports.config = {
 
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "ⁱᵃᵐ|¸„.-•~¹°”ˆ˜¨ť𝒆αꪑx𝖉αȵᶢ𝒆𝔯¨˜ˆ𒆜",
 	description: "Comment on table ( ͡° ͜ʖ ͡°)",
 	commandCategory: "edit-img",
 	usages: "google [text]",
