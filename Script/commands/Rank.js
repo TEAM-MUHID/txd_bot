e97a1c925b8739514d5f4d0af338b77c71948755
I.A.M.S <islammuhid962@gmail.com>
2025-09-08 15:14:27 +0600

    Update Rank.js

--- Script/commands/Rank.js
+++ Script/commands/Rank.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "rank",
 	version: "2.0.0",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "ⁱᵃᵐ|¸„.-•~¹°”ˆ˜¨ť𝒆αꪑx𝖉αȵᶢ𝒆𝔯¨˜ˆ𒆜",
 	description: "View Member Rankings",
 	commandCategory: "Group",
 	usages: " [user] or [tag]",
